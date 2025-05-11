441d6772686cbb8425dfceeb4806ae9e3439927d
Huda-Kausar17 <hudakausar2517@gmail.com>
2025-05-11 19:54:53 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,8 +2,8 @@ public class demo {
 	public static void main(String []args) {
 		int a=100;
 		int b=20;
-		int c=a-b;
-		System.out.println("The difference is = "+c);
+		int c=a+b;
+		System.out.println("The sum is = "+c);
 	}
 
 }
